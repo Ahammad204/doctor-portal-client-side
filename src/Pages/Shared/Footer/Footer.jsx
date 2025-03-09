26a3c7a3cf9ae1977f392ba5e54100c9c9eb46b8
Kazi Ahammad Ullah <ahammadcode@gmail.com>
2025-03-09 17:55:08 +0600

    responsive navbar and footer

---
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import footerbg from '../../../../public/assets/images/footer.png'
+
+const Footer = () => {
+  return (
+    <footer
+    style={{ backgroundImage: `url(${footerbg})` }}
+    className="bg-cover bg-center "
+  >
+      <div className="footer sm:footer-horizontal   p-10">
+        <nav>
+          <h6 className="footer-title">Services</h6>
+          <a className="link link-hover">Emergency Checkup</a>
+          <a className="link link-hover">Monthly Checkup</a>
+          <a className="link link-hover">Weekly Checkup</a>
+          <a className="link link-hover">Deep Checkup</a>
+        </nav>
+        <nav>
+          <h6 className="footer-title">ORAL HEALTH</h6>
+          <a className="link link-hover">Fluoride Treatment</a>
+          <a className="link link-hover">Cavity Filling</a>
+          <a className="link link-hover">Teath Whitening</a>
+        </nav>
+        <nav>
+          <h6 className="footer-title">OUR ADDRESS</h6>
+          <a className="link link-hover">New York - 101010 Hudson</a>
+        </nav>
+      </div>
+      <div className="footer sm:footer-horizontal footer-center  p-4">
+        <aside>
+          <p>
+            Copyright © {new Date().getFullYear()} - All right reserved 
+          </p>
+        </aside>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
